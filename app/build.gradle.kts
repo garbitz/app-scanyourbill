302471893aac1b0183163ee15b9aa9a163fd5cf9
Trias Ahmad Fairuz <trias3011@gmail.com>
2024-06-14 14:06:26 +0700

    fix wrong id, drawables. Add font. Start login activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-base:17.6.0");
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
