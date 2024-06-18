9d566b60417457a2bf2bdb62a7648e96fcd28d95
Trias Ahmad Fairuz <trias3011@gmail.com>
2024-06-18 23:23:19 +0700

    feat: setup search layout

--- app/src/main/java/com/example/scanyourbill/view/search/SearchActivity.kt
+++ app/src/main/java/com/example/scanyourbill/view/search/SearchActivity.kt
@@ -3,12 +3,16 @@ package com.example.scanyourbill.view.search
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.scanyourbill.R
+import com.example.scanyourbill.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
 
-        setContentView(R.layout.activity_search)
+        setContentView(binding.root)
 
     }
 }
